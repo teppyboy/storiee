f2dcc6fccc49aa4e1351e8dc249c33853fb90383
tretrauit <tretrauit@gmail.com>
2024-02-17 21:35:16 +0700

    chore: use msedge-beta
    Chrome doesn't provide arm64 binary wtf

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -32,7 +32,7 @@ logger.info("Initializing browser...");
 // Launch the browser and open a new blank page
 const browser = await chromium.launch({
 	headless: process.env.HEADLESS !== "false" ? true : false,
-	channel: "chrome",
+	channel: "msedge-beta",
 });
 
 facebook.setBrowser(browser);
